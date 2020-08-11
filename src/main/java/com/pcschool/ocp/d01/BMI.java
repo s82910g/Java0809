6bfc730482fd766b7d4218a6040e9cbd2bdb41f5
MB-study <MB-study@192.168.1.101>
2020-08-11 11:09:33 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/BMI.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.d01;
+
+public class BMI {
+    public String name;
+    public double h;
+    public double w;
+    public double bmi;
+    
+    //計算BMI方法
+    public double calcBMI(){
+        bmi=w/Math.pow(h/100, 2);
+        return bmi;
+    }
+}
